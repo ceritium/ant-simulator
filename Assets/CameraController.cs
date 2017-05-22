ba4632041dc5e79d449dd2120ef03e087a7618c3
Jose Galisteo <ceritium@gmail.com>
2017-05-22 14:35:35 +0200

    scroll zoom

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+	public int cameraCurrentZoom = 50;
+	public int cameraZoomMin = 20;
+
 	// Use this for initialization
 	void Start () {
-		
+		Camera.main.orthographicSize = cameraCurrentZoom;		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+			if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+			{
+					cameraCurrentZoom += 1;
+					Camera.main.orthographicSize = Mathf.Max(Camera.main.orthographicSize + 1);
+
+			}
+			if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+			{
+				if (cameraCurrentZoom > cameraZoomMin)
+				{
+					cameraCurrentZoom -= 1;
+					Camera.main.orthographicSize = Mathf.Min(Camera.main.orthographicSize - 1);
+				}   
+			}
+
+
+
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
 			Vector3 position = this.transform.position;
